7685e4341d5ece5b9639c649445f293d5174c723
Dracape <SomilGoyal2010@outlook.com>
2025-09-03 00:18:39 +0530

    CD into layouts directly

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 
 PREV_DIR=$(pwd)
 REPONAME='modANSI'
-GRAPHENE_INSTALL_SCRIPT_DIR=layouts/graphene/install
-MIDNIGHT_INSTALL_SCRIPT_DIR=layouts/midnight/install
+GRAPHENE_INSTALL_SCRIPT_DIR=graphene/install
+MIDNIGHT_INSTALL_SCRIPT_DIR=midnight/install
 
 cd /tmp
 
 rm -rf "$REPONAME"
 git clone https://github.com/Dracape/"$REPONAME".git
-cd "$REPONAME"
+cd "$REPONAME"/layouts
 
 # Make the scripts executable
 chmod +x "$GRAPHENE_INSTALL_SCRIPT_DIR"/install.sh "$MIDNIGHT_INSTALL_SCRIPT_DIR"/install.sh
